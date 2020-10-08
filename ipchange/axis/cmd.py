fe8f886fa154ee969a886108606e90c93c61c95f
Stuart B. Wilkins <swilkins@bnl.gov>
2020-10-08 07:16:22 -0400

    Add change password

--- ipchange/axis/cmd.py
+++ ipchange/axis/cmd.py
@@ -5,7 +5,7 @@ import ipaddress
 import ipchange.moxa
 
 
-def axis_changeip():
+def axis_change_ip():
 
     parser = argparse.ArgumentParser(
         prog=os.path.basename(sys.argv[0]),
@@ -62,6 +62,61 @@ def axis_changeip():
         str(args['New IP Address']),
         str(args['New Netmask']),
         str(args['New Gateway'])
+    )
+
+    return 0
+
+
+def axis_change_pwd():
+
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        description='Change Password of AXIS Webcam'
+    )
+
+    parser.add_argument(
+        'AXIS Webcam address/hostname',
+        metavar='host', type=str,
+        help='Address or Hostname of the AXIS Webcam to change'
+    )
+
+    parser.add_argument(
+        'Username to change'
+        metavar='username', type=str,
+        help='Username of the user to change password for'
+    )
+
+    parser.add_argument(
+        'Password to change'
+        metavar='host', type=str,
+        help='New password for given user"
+    )
+
+    parser.add_argument(
+        '-u', '--username', dest='username', action='store',
+        help='Username of the AXIS Webcam to connect to',
+        default=None
+    )
+
+    parser.add_argument(
+        '-p', '--password', dest='password', action='store',
+        help='Password of the AXIS Webcam to connect to',
+        default=None
+    )
+
+    args = vars(parser.parse_args())
+
+    # Now run the set routine
+
+    axis = ipchange.axis.AxisWebcam(
+        args['AXIS Webcam address/hostname'],
+        username=args['username'],
+        password=args['password']
+    )
+
+    axis.change_passwd(
+        username=args['Username to change']),
+        password=args['password to change'])
     )
 
     return 0
